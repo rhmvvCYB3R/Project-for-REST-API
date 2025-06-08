4d09a6d26607717119f7f04d081f7f986673483a
rhmvvCYB3R <10ragimov10@gmail.com>
2025-06-08 23:10:39 +0200

    Добавлен файл для докера

--- backend-dotnet/obj/Release/net9.0/REST project.AssemblyInfo.cs
+++ backend-dotnet/obj/Release/net9.0/REST project.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("REST project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d55b1555d8677234fab854376cd0fee7eddb511")]
 [assembly: System.Reflection.AssemblyProductAttribute("REST project")]
