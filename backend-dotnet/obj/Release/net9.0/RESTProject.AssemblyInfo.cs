66a6db65ce9f1e698b3351d4e90398120757b0ed
rhmvvCYB3R <10ragimov10@gmail.com>
2025-06-09 00:13:24 +0200

    Cccc

--- backend-dotnet/obj/Release/net9.0/RESTProject.AssemblyInfo.cs
+++ backend-dotnet/obj/Release/net9.0/RESTProject.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc94697895743faba066b4c86be7d503a9fde4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTProject")]
